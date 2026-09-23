32772c44366021c1c767883a181e3a5b8ba15f4e
Werner07 <wsacbas @miumg.edu.gt>
2026-09-23 00:06:45 -0600

    actualizacion de mensaje

--- src/holagit/Holagit.java
+++ src/holagit/Holagit.java
@@ -14,7 +14,7 @@ public class Holagit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-     System.out.println("Hola Mundo");   // TODO code application logic here
+     System.out.println("Hola werner sacba");   // TODO code application logic here
     }
     
 }
